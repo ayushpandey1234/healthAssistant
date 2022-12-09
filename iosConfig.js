75b8f9b250ca180d4bc7ae48b8a3dd7cbc1bf891
Navneet Das <navneet.d@ayu.health>
2022-12-09 20:47:52 +0530

    Hackathon: Adds extra properties average sum diff

--- iosConfig.js
+++ iosConfig.js
@@ -6,6 +6,11 @@ const permissions = {
       AppleHealthKit.Constants.Permissions.StepCount,
       AppleHealthKit.Constants.Permissions.ActiveEnergyBurned,
       AppleHealthKit.Constants.Permissions.Workout,
+      AppleHealthKit.Constants.Permissions.HeartRate,
+      AppleHealthKit.Constants.Permissions.BloodPressureSystolic,
+      AppleHealthKit.Constants.Permissions.BloodPressureDiastolic,
+      AppleHealthKit.Constants.Permissions.BodyTemperature,
+      AppleHealthKit.Constants.Permissions.BloodGlucose,
     ],
   },
 };
@@ -16,20 +21,112 @@ AppleHealthKit.initHealthKit(permissions, error => {
   }
 });
 
-const appleHealthKitOptions = {
-  startDate: new Date().toISOString(),
+function averageValueOfResults(results) {
+  let totalValue = 0;
+  let countOfValues = 0;
+  results.forEach(result => {
+    totalValue += result.value;
+  });
+  return Math.round(totalValue / countOfValues);
+}
+
+function averageValueOfBloodPressureResults(results) {
+  let totalSystolicValue = 0;
+  let totalDiastolicValue = 0;
+  let countOfValues = 0;
+  results.forEach(result => {
+    totalSystolicValue += result.bloodPressureSystolicValue;
+    totalDiastolicValue += result.bloodPressureDiastolicValue;
+  });
+  return (
+    Math.round(totalSystolicValue / countOfValues),
+    Math.round(totalDiastolicValue / countOfValues)
+  );
+}
+
+function getMinutesDifferenceBetweenTwoDates(startDate, endDate) {
+  return Math.round((endDate.getTime() - startDate.getTime()) / 60000);
+}
+
+function aggregateCaloriesFromSamples(results) {
+  let totalCalories = 0;
+  results.forEach(result => {
+    totalCalories += result.calories;
+  });
+  return totalCalories;
+}
+
+function aggregateMoveMinutesFromSamples(results) {
+  let totalMoveMinutes = 0;
+  results.forEach(result => {
+    totalMoveMinutes += getMinutesDifferenceBetweenTwoDates(
+      result.start,
+      result.end,
+    );
+  });
+  return totalMoveMinutes;
+}
+
+const optionsWithStartDate = {
+  startDate: new Date(2022, 12, 9).toISOString(),
 };
 
-AppleHealthKit.getStepCount(appleHealthKitOptions, (err, results) => {
+AppleHealthKit.getStepCount(optionsWithStartDate, (err, results) => {
   if (err) {
     return;
   }
-  console.log(results);
+  console.log('Total Steps: ' + results.value);
 });
 
-AppleHealthKit.getActiveEnergyBurned(appleHealthKitOptions, (err, results) => {
+AppleHealthKit.getActiveEnergyBurned(optionsWithStartDate, (err, results) => {
   if (err) {
     return;
   }
-  console.log(results);
+  console.log('Total Active Energy Burned: ' + results.value);
+});
+
+AppleHealthKit.getSamples(optionsWithStartDate, (err, results) => {
+  if (err) {
+    return;
+  }
+  console.log('Total Calories: ' + aggregateCaloriesFromSamples(results));
+  console.log('Total Move Minutes:' + aggregateMoveMinutesFromSamples(results));
+});
+
+AppleHealthKit.getBodyTemperatureSamples(
+  optionsWithStartDate,
+  (err, results) => {
+    if (err) {
+      return;
+    }
+    console.log('Average Body Temperature: ' + averageValueOfResults(results));
+  },
+);
+
+AppleHealthKit.getHeartRateSamples(optionsWithStartDate, (err, results) => {
+  if (err) {
+    return;
+  }
+  console.log('Average Heart Rate: ' + averageValueOfResults(results));
+});
+
+AppleHealthKit.getBloodPressureSamples(optionsWithStartDate, (err, results) => {
+  if (err) {
+    return;
+  }
+  let [averageSystolicValue, averageDiastolicValue] =
+    averageValueOfBloodPressureResults(results);
+  console.log(
+    'Average Blood Pressure Value (Systolic/Diastolic): ' +
+      averageSystolicValue +
+      '/' +
+      averageDiastolicValue,
+  );
+});
+
+AppleHealthKit.getBloodGlucoseSamples(optionsWithStartDate, (err, results) => {
+  if (err) {
+    return;
+  }
+  console.log('Average Blood Glucose: ' + averageValueOfResults(results));
 });
